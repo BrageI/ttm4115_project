72a0d1f0606b6b8e25ba7d63dda7bb18a6953be3
KalleNewall <k.newall1@gmail.com>
2024-04-09 22:33:36 +0200

    Can now park cars, no charge rates etc yet

--- data_collection.py
+++ data_collection.py
@@ -9,12 +9,6 @@ class Color:
     red = [255, 0, 0] #Color for occupied charger
     green = [0, 255, 0] #Color for free charger
 
-# def toggle_color(color):
-#     if color == Color.red:
-#         color = Color.green
-#     else: 
-#         color = Color.red
-
 def get_charger_pixels_from_top_left_pixel(top_left_pixel):
     x, y = top_left_pixel
     # Define the relative movements from the top-left pixel
@@ -45,6 +39,20 @@ def render_led_matrix(location):
     for charger in location.chargers:
         render_charger(charger)
         
+def find_free_charger_number(location):
+    free_charger_number = 0
+    for charger in location.chargers:
+        if charger.status == Charger.Status.NO_CAR:
+            return free_charger_number
+        free_charger_number+=1
+    return -1
+
+def park_car(location):
+    free_charger_number = find_free_charger_number(location)
+    if free_charger_number != -1:
+            location.chargers[free_charger_number].toggle_status()
+    render_led_matrix(location)
+        
 #location.chargers[0].status = Charger.Status.NO_CAR
 
 sense.clear()
@@ -53,8 +61,7 @@ render_led_matrix(location)
 while True:
     for event in sense.stick.get_events():
         if event.action == "released":
-            location.chargers[0].toggle_status()
-            render_led_matrix(location)
+            park_car(location)
             
     
     
