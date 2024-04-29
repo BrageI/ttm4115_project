84f0cb2f8e15206fa8a158173f0c3d551be9313a
Brage Imset <Braimset@gmail.com>
2024-04-29 13:29:41 +0200

    Make server mvp

---
+++ server.py
@@ -0,0 +1,45 @@
+import pickle
+import json
+
+import paho.mqtt.client as mqtt
+from shared.charger_data import Charger
+
+import shared.mqtt_opts
+
+class Server:
+    def __init__(self) -> None:
+        self.mqtt_client = mqtt.Client()
+        self.mqtt_client.on_connect = self.on_mqtt_connect
+        self.mqtt_client.on_message = self.on_mqtt_message
+        self.mqtt_client.connect(shared.mqtt_opts.BROKER, shared.mqtt_opts.PORT)
+
+        self.mqtt_client.subscribe("ttm4115/gruppe21/fromstation")
+
+    def on_mqtt_connect(self, client, userdata, flags, rc):
+        print("on_mqtt_connect(): {}".format(mqtt.connack_string(rc)))
+
+    def on_mqtt_message(self, client, userdata, msg: mqtt.MQTTMessage):
+        print("on_mqtt_message(): topic: {}".format(msg.topic))
+        parsed: list[Charger.Data] = pickle.loads(msg.payload)
+        
+        available_now = len([0 for c in parsed if c.status == Charger.Status.NO_CAR])
+        available_on_arrival = available_now
+        for charger in parsed:
+            if charger.status == Charger.Status.CHARGING and charger.charge_percentage > 75.0:
+                available_on_arrival += 1
+
+        out = {
+            "available_chargers": available_now,
+            "available_chargers_arrival": available_on_arrival,
+            "total_chargers": len(parsed),
+            "time_until_arrival": 15.0
+        }
+
+        self.mqtt_client.publish(f"ttm4115/gruppe21/fromserver", json.dumps(out))
+    
+        
+
+
+server = Server()
+
+server.mqtt_client.loop_forever()
